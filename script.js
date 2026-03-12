08c14c33ff9c06f9ce8e342d2e0a50eea01006d5
Kennet Rodriguez <kennet_rl@hotmail.com>
2026-03-11 21:11:57 -0500

    fix(Navbar): Tamaños y colores

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ domLineasDropbtn.addEventListener('click', toggleServiciosMobile);
 
 const isIndex = window.location.pathname === '/' || window.location.pathname.endsWith('index.html');
 
-function toggleMenu(){
+function toggleMenu() {
     domLineas.classList.toggle('inactive');
-    if(isIndex){
-        if(domLineas.classList.contains('inactive')){
-            menuLineas.src = './icons/menu1.webp';
+    if (isIndex) {
+        if (domLineas.classList.contains('inactive')) {
+            menuLineas.src = './icons/menu.webp';
         } else {
             menuLineas.src = './icons/menu.webp';
         }
     }
 }
 
-function toggleServiciosMobile(){
+function toggleServiciosMobile() {
     domLineasDropdownContent.classList.toggle('inactive');
     domLineasDropbtn.classList.toggle('open');
 }
